f41a76252533e545695f3dcf95df7e3879e09524
haoxiangpeng <haoxiangpeng@hotmail.com>
2017-03-14 16:38:10 +0800

    some changes

--- src/components/MIPS.vue
+++ src/components/MIPS.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="hello">
-    <input id="file-upload" @change="onFileChange" accept="text/*" type="file" >
-    <ol v-if="fileLineList">
-      <div v-for="line in fileLineList" contenteditable="true" v-html="line"></div>
-    </ol>
-    <li v-html="htmlTemp"></li>
+    <input id="file-upload" @change="onFileChange" accept="*" type="file" >
+    <el-row>
+      <el-col :span="12">
+      <div v-if="fileLineList">
+        <el-row v-for="(line, index) in fileLineList" :key="index">
+          <el-col :span="1" style="background-color:#34495e; color:#ecf0f1; text-align:right;padding-right:4px;">{{index}}.</el-col>
+          <el-col :span="23" style="background-color:#3f556b" contenteditable="true" v-html="line"></el-col>
+        </el-row>
+      </div>
+      </el-col>
+      <el-col :span="12">
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -18,7 +26,8 @@ export default {
       file: '',
       htmlTemp: '',
       instructRegx: '(\\s*)?(\\w+)(\\s+)(\\$\\w+)(\\s*)?(,)(\\s*)?(\\$\\w+)(\\s*)?(,)(\\s*)?(\\$\\w+)(\\s*)?(;)(\\s*)(\\/\\/.*)?',
-      commentRegx: '(\\s*)(\\/\\/.*)'
+      commentRegx: '(\\s*)(\\/\\/.*)',
+      labelRegx: '(\\s*)(\\.\\w+)(\\s+)(\\w+)(\\s*)(\\/\\/.*)?'
     }
   },
   filters: {
@@ -38,7 +47,6 @@ export default {
       let files = e.target.files || e.dataTransfer.files
       if (files.length === 1) {
         self.file = files[0]
-        console.log('asdf')
       } else {
         return
       }
@@ -54,12 +62,14 @@ export default {
       let self = this
       let insRegx = new RegExp(self.instructRegx)
       let commRegx = new RegExp(self.commentRegx)
+      let labelRegx = new RegExp(self.labelRegx)
       let stringList = insRegx.exec(line)
       let commList = commRegx.exec(line)
-      console.log(stringList)
-      console.log(commList)
+      let labelList = labelRegx.exec(line)
       if (stringList) {
         return self.getSyntax(stringList)
+      } else if (labelList) {
+        return self.getSyntax(labelList)
       } else if (commList) {
         return self.getSyntax(commList)
       } else {
@@ -68,13 +78,17 @@ export default {
     },
     getSyntax: function (valueList) {
       let result = ''
-      console.log(valueList)
       for (let i = 1; i < valueList.length; i++) {
         if (!valueList[i]) continue
         if (valueList[i].includes('//')) {
           result += ('<span style="color: #bdc3c7">' + valueList[i] + '</span>')
+        } else if (valueList[i].includes('.')) {
+          console.log(valueList[i])
+          result += ('<span style="color: #27ae60">' + valueList[i] + '</span>')
         } else if (valueList[i].includes('$') || valueList[i].includes('x')) {
           result += ('<span style="color: #e67e22">' + valueList[i] + '</span>')
+        } else if (valueList[i].includes('x')) {
+          result += ('<span style="color: #2980b9">' + valueList[i] + '</span>')
         } else if (valueList[i].includes(',') || valueList[i].includes(';')) {
           result += ('<span style="color: #8e44ad">' + valueList[i] + '</span>')
         } else {
