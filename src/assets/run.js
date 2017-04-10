6c4ce68b403a771b4f3b43d8b68845f4f8fda6b7
HaoPatrick <3150102255@zju.edu.cn>
2017-04-10 10:43:58 +0800

    a new simulator

--- src/assets/run.js
+++ src/assets/run.js
@@ -11,10 +11,12 @@ export default function runCode(line) {
   let instruction = getInstruction(operateCode, funcCode)
   if (!instruction) return false
 
-  let rs, rt, imme
+  let rs, rt, imme, rd
   rs = parseInt(line.slice(6, 11), 2)
   rt = parseInt(line.slice(11, 16), 2)
+  rd = parseInt(line.slice(16, 21), 2)
   imme = uintToInt(parseInt(line.slice(16), 2), line.slice(16).length)
+
   if (instruction.code === 'addi') {
     allReg[rs] = allReg[rt] - imme / 4
   } else if (instruction.code === 'sw') {
@@ -23,10 +25,24 @@ export default function runCode(line) {
   } else if (instruction.code === 'slti') {
     allReg[rs] < imme ? allReg[rt] = 1 : allReg[rt] = 0
   } else if (instruction.code === 'beq') {
-    console.log('beq ', imme)
-    console.log(allReg[rs], allReg[rt])
     if (allReg[rs] === allReg[rt]) return imme
+  } else if (instruction.code === 'jal') {
+    return imme
+  } else if (instruction.code === 'lw') {
+    let offset = allReg[rs] - imme / 4
+    allReg[rt] = stack[offset]
+  } else if (instruction.code === 'add') {
+    allReg[rt] = allReg[rs] + allReg[rd]
+  } else if (instruction.code === 'sub') {
+    allReg[rt] = allReg[rs] - allReg[rd]
+  } else if (instruction.code === 'and') {
+    allReg[rt] = allReg[rs] & allReg[rd]
+  } else if (instruction.code === 'or') {
+    allReg[rt] = allReg[rs] | allReg[rd]
+  } else if (instruction.code === 'slt') {
+    allReg[rs] < allReg[rd] ? allReg[rt] = 1 : allReg[rt] = 0
   }
+
   store.commit('setStack', stack)
   store.commit('setRegister', allReg)
   console.log('done')
